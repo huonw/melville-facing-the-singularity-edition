fed88e9e5055dc1d7083ee8b310a47d2bafcaad2
Huon Wilson <dbau.pp+github@gmail.com>
2011-11-26 17:20:53 +1100

    First modifications for facingthesingularity.com

--- single.php
+++ single.php
@@ -11,10 +11,15 @@ get_header(); ?>
 						
 						<?php the_content(); ?>
 				<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
+				
+				<p class="next-chapter"><?php next_post_link('%link','Next chapter &rarr;'); ?></p>
+
+				<?php if (FALSE) : /* dont show any of this next stuff */ ?>
 				<p class="date"><?php twentyten_posted_on(); ?></p>
 				<p class="post-meta"><?php twentyten_posted_in();?>
 
 				<?php comments_template( '', true ); ?>
+				<?php endif; ?>
 
 <?php endwhile; // end of the loop. ?>
 </div>

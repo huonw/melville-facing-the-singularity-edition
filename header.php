63ee66f25767c4e46c335c67753ff4bf664dcd3c
Huon Wilson <dbau.pp+github@gmail.com>
2011-12-28 18:38:45 +1100

    Header image links to site root

--- header.php
+++ header.php
@@ -63,7 +63,11 @@
 <body <?php body_class(); ?>>
 <div id="wrapper">
 	<div id="header">
-	       <img id="header-image" src="<?php bloginfo('stylesheet_directory') ?>/images/header.png" />
+	    <div>
+	       <a href="/">
+	          <img id="header-image" src="<?php bloginfo('stylesheet_directory') ?>/images/header.png" />
+               </a>
+            </div>
 		<?php $heading_tag = ( is_home() || is_front_page() ) ? 'h1' : 'div'; ?>
 				<<?php echo $heading_tag; ?> id="site-title">
 						<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
